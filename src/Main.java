dbd50c3c88ecc94e2c98aca29807bedc1ad4f59b
Karina Marusik <kamarusik@sberbank.ru>
2023-05-17 16:51:33 +0300

    delete README.md, refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
-
     public static void main(String[] args) {
-        int eaters = 5;
 
-        int water = 3000;
-        int potatoes = 5;
-        int chicken = 6;
-        int spices = 10;
+        int eaters = 5; // сколько людей будут есть
 
-        System.out.println("Сварил суп. На одного вышло:");
-        System.out.println((water + eaters) + "Миллилитров воды");
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров воды");
         System.out.println((potatoes / eaters) + " картофелин(а)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
-    }
 
+    }
 }
